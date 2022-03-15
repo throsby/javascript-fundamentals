891fbdec952fc6aa928e209f9627ef40043c1545
Throsby Wells <throsbywells@gmail.com>
2022-03-15 15:31:40 -0400

    added functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Javascript is running!")
+let score = 100
+
+
+alert(score)
+let h1 = document.createElement('h1')
+h1.innerText = "This came from javascript"
+
+document.body.appendChild(h1)
---
+++ module.js
@@ -0,0 +1,16 @@
+/* 
+Function allow to:
+    1. Bundle together a lot of functionality 
+    2. Easily control when it is applied
+
+*/
+
+
+let myName = prompt("Enter your name")
+let month = prompt("What month were you born in?")
+let food = prompt("What is your favorite food?")
+
+console.log("My name is ", myName)
+console.log("I was born in ", month)
+console.log("My favorite food is ", food)
+
