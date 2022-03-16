51e5e995ea822de79d5a5ec98ebf94754f5bbcee
Throsby Wells <throsbywells@gmail.com>
2022-03-16 13:31:13 -0400

    filled out rote.js

---
+++ rote.js
@@ -0,0 +1,18 @@
+const greet = () => {
+    console.log("Hello world")
+}
+
+greet()
+
+const square = (number) => {
+    return number * number
+}
+
+square(25)
+
+const greetByName = (str) => {
+    console.log("Hello,", str)
+}
+
+greetByName("Throsby")
+
