101fdddb220c85c1c7752455d395ec869ae531c6
Throsby Wells <throsbywells@gmail.com>
2022-03-17 13:22:51 -0400

    added tenants

--- apartments/index.js
+++ apartments/index.js
@@ -5,6 +5,7 @@ let apt1 = {
     windows: 0,
     rent: 4500,
     unit: "1A",
+    tenants: [],
 }
 
 let apt2 = {
@@ -13,7 +14,8 @@ let apt2 = {
     bathrooms: 1,
     windows: 2,
     rent: 1500,
-    unit: "1B"
+    unit: "1B",
+    tenants: [],
 }
 
 let apt3 = {
@@ -22,7 +24,32 @@ let apt3 = {
     bathrooms: 3,
     windows: 2,
     rent: 2400,
-    unit: "1C", 
+    unit: "1C",
+    tenants: [],
+}
+
+let apt4 = {
+    bedrooms: 0, 
+    sqft: 11, 
+    bathrooms: 0,
+    windows: 0,
+    rent: 0,
+    unit: "1D", 
+    tenants: [],
+}
+
+let tenant1 = {
+    name: "John Doe",
+    creditScore: 400,
+    salary: 15000,
+    age: 30,
+}
+
+let tenant2 = {
+    name: "Jane Doe",
+    creditScore: 800,
+    salary: 95000,
+    age: 30,
 }
 
 let building = {
