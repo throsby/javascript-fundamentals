d35d32ca4e19f9637e4f701c0b84569aa641730d
Throsby Wells <throsbywells@gmail.com>
2022-03-17 12:02:36 -0400

    added apartments to building

---
+++ apartments/index.js
@@ -0,0 +1,34 @@
+let apt1 = {
+    bedrooms: 2, 
+    sqft: 50, 
+    bathrooms: 3,
+    windows: 0,
+    rent: 4500,
+    unit: "1A",
+}
+
+let apt2 = {
+    bedrooms: 1,
+    sqft: 500,
+    bathrooms: 1,
+    windows: 2,
+    rent: 1500,
+    unit: "1B"
+}
+
+let apt3 = {
+    bedrooms: 2, 
+    sqft: 51, 
+    bathrooms: 3,
+    windows: 2,
+    rent: 2400,
+    unit: "1C", 
+}
+
+let building = {
+    streetAddress: "11 Broadway",
+    laundry: false,
+    allowsPets: false,
+    apartments: [apt1, apt2, apt3],
+}
+
