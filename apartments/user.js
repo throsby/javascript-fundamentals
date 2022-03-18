f0574ca6341b8175b770a0fb109d1db739cd35b5
Throsby Wells <throsbywells@gmail.com>
2022-03-18 11:40:36 -0400

    added character and other objects

--- apartments/user.js
+++ apartments/user.js
@@ -1,53 +1,55 @@
-// Write a basic JavaScript object that represents a user that has no fewer than 3 keys/properties about that user
-
-const user = {
-    name: "John Potomsher",
-    ip: "100.400.2.1",
-    email: "willywaller@gmail.com"
+const laptop = {
+    price: 1000,
+    brand: "HP",
+    memory: "2GB",
+    color: "pink"
 }
 
-// Write a function called greet that takes no arguments and prints the string "Hello World"
-
-const greet = () => {
-    console.log("Hello World")
+const car = {
+    engine: 'V8',
+    doors: 2,
+    model: "BMW",
+    price: 200000,
+    make: "M4"
 }
 
-// Create an array called fruits with 5 elements
-
-const fruits = ["apple", "banana", "passionfruit", "jackfruit", "blueberry"]
-
-
-// console.log the second element and console.log the last element of the above array
-console.log(fruits[1])
-console.log(fruits[fruits.length-1])
-
-
-// Create an object that represents an apartment building without referring to any code
-
-
-const apt1 = {
-    floor: 1,
-    southFacing: true,
-    nearElevator: true,
-    rent: 1500,
+const company = {
+    name: "Vittles",
+    ceo: "Michael Law",
+    isTheBest: true,
+    publiclyTraded: false,
 }
 
-const apartmentB = {
-    lobby: {
-        lobbySqFt: 2000,
-        lobbyFloorMaterial: "Italian Marble"
-    },
-    floors: 7,
-    rooms: 18,
-    roomsArray: [apt1],
-    supplyRooms: 3,
+const room = {
+    windows: 0,
+    dimensions: '200x200',
+    closet: false,
+    isLegal: false,
+    pets: false
+}
+
+const item = {
+    name: "Excalibur",
+    attackPower: 130,
 
 }
 
+const ally = {
+    name: "Geralt",
+    power: { name: "Kamehameha", attackPower: 9000}
+}
 
+const character = {
+    hairColor: 'blue',
+    height: "6'7",
+    race: 'Elf',
+    gender: null,
+    strength: Infinity,
+    dlc: false,
+    class: "Mage",
+    cohort: "031422",
+    item: item,
+    ally: ally,
+}
 
-
-
-// greet()
-// console.log(user)
-// console.log(fruits)
+console.log(character)
